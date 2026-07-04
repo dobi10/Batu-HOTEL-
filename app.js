39fb03c6d7d9101299c5fa81c8e14d2a913e6a6e
dobi10 <dozohi1029@gmail.com>
2026-07-04 21:51:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
+// 🔥 Your Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDhsqdbW9C4e_wQ1XCkjghmlUnugCV1S-s",
+  authDomain: "batu-hotel-f00fd.firebaseapp.com",
+  projectId: "batu-hotel-f00fd",
+  storageBucket: "batu-hotel-f00fd.appspot.com",
+  messagingSenderId: "60513617208",
+  appId: "1:60513617208:web:5d8d3020b95d048144edcd"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Services (used by other files)
 export const db = getFirestore(app);
 export const auth = getAuth(app);
